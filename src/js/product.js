5c897923c4ddaeb880733d6eaa9c0b7440107e28
trrmann <trmann70@hotmail.com>
2026-09-07 22:36:35 -0400

    render test

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  const cartItems = getLocalStorage('so-cart') || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage('so-cart', cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -17,8 +17,8 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
 
 //Changelog: Zachary P Newby
 //9.5.26
