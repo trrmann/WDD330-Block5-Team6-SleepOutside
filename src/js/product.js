06327dfc79fa31ffdd9501531b6349e86ac20da9
Zach Newby <zpnewby@outlook.com>
2026-09-09 18:47:52 -0500

    Team activity progress

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,14 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
+
+const productId = getParam('product');
 const dataSource = new ProductData('tents');
+
+const product = new ProductDetails(productId, dataSource);
+
+product.init();
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage('so-cart') || []; // get cart array of items from local storage if null set to empty array
